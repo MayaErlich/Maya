3bd7eaa92c5ad3ab7207f5f5cfb4c4277e0dc3ec
MayaErlich <mayaluciaerlich@gmail.com>
2022-05-10 09:13:47 -0400

    started ui, made buttons and label, made gridpane layout

--- src/main/java/com/example/maya/App.java
+++ src/main/java/com/example/maya/App.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/example/maya/fxml/view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Course Management");
         stage.setScene(scene);
         stage.show();
     }
